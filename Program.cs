a4fbe18b75445f67ffc0e6cdb6ef7542e7a93553
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-01-25 08:38:29 +0000

    added more operations, fixed class name

--- Program.cs
+++ Program.cs
@@ -1,34 +1,54 @@
-class Adder 
+class Calc
 {
-	public int a { get; set; }
-	public int b { get; set; }
+    public int a { get; set; }
+    public int b { get; set; }
 
-	public Adder(int a,int b) 
-	{
-		this.a = a;
-		this.b = b;
-	}
+    public Calc(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
 
-	public int Add()
-	{
-		return a + b;
-	}
+    public int Operations(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                return this.a + this.b;
+
+            case 2:
+                return this.a - this.b;
+
+            case 3:
+                return this.a * this.b;
+
+            case 4:
+                return this.a / this.b;
+        }
+
+        return 0;
+    }
 
 }
 
-class Program 
+class Program
 {
-	static void Main( string[] args )
-	{
-		Console.WriteLine("What's the first Number?");
-		int answer1 = Convert.ToInt32(Console.ReadLine());
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What's the first Number?");
+        int answer1 = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("What's the second number?");
-		int answer2 = Convert.ToInt32(Console.ReadLine());
-		
-		Adder adder = new Adder( answer1, answer2 );
-		int result = adder.Add();
+        Console.WriteLine("What's the second number?");
+        int answer2 = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine( result );
-	}
+        Console.WriteLine("What Operation?\n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division");
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        Calc Calculator = new Calc(answer1, answer2);
+
+        int result = Calculator.Operations(choice);
+
+        Console.WriteLine(result);
+    }
 }
